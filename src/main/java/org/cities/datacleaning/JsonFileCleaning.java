10b77f8f650cd858cb7b45bb5e1c68c80e9e50c2
Carlos Santiago <carlos.san.rol@gmail.com>
2017-05-07 16:29:32 +0200

    First commit

---
+++ src/main/java/org/cities/datacleaning/JsonFileCleaning.java
@@ -0,0 +1,268 @@
+package org.cities.datacleaning;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Carlos on 30/04/2017.
+ */
+public class JsonFileCleaning {
+    private static final String API_KEY = "6ab6acff-85f6-4580-b127-32067e5667ab";
+    private static final String BASE_URL = "https://euw1.api.riotgames.com/api/lol/euw/";
+    private static int requests = 0;
+
+    public static void main(String[] args) {
+        if( args.length != 2 ){
+            usage();
+        }
+
+        String file_name = args[0];
+        String out_file_name = args[1];
+        PrintWriter writer;
+        try {
+            writer = new PrintWriter(new File(out_file_name));
+            writer.println("Team1Win,Champ1WR,Champ2WR,Champ3WR,Champ4WR,Champ5WR,Champ6WR,Champ7WR,Champ8WR,Champ9WR,Champ10WR,PlayerChamp1WR,PlayerChamp2WR,PlayerChamp3WR,PlayerChamp4WR,PlayerChamp5WR,PlayerChamp6WR,PlayerChamp7WR,PlayerChamp8WR,PlayerChamp9WR,PlayerChamp10WR,Player1ChampTimes,Player2ChampTimes,Player3ChampTimes,Player4ChampTimes,Player5ChampTimes,Player6ChampTimes,Player7ChampTimes,Player8ChampTimes,Player9ChampTimes,Player10ChampTimes,Player1Ranking,Player2Ranking,Player3Ranking,Player4Ranking,Player5Ranking,Player6Ranking,Player7Ranking,Player8Ranking,Player9Ranking,Player10Ranking");
+        } catch (Exception e) {
+            System.out.println("Error creating printwriter");
+            return;
+        }
+
+
+        //go through each file
+        String json_string = "";
+        String champion_json_string = "";
+        try{
+            List<String> lines = Files.readAllLines( Paths.get( file_name ), Charset.forName("ISO-8859-1") );
+            ListIterator<String> lines_iterator = lines.listIterator();
+            while( lines_iterator.hasNext() ){
+                json_string += lines_iterator.next();
+            }
+
+            List<String> champ_lines = Files.readAllLines( Paths.get( "ChampionIDWinRates.json" ), Charset.forName( "UTF-8" ) );
+            ListIterator<String> champ_lines_iterator = champ_lines.listIterator();
+            while( champ_lines_iterator.hasNext() ){
+                champion_json_string += champ_lines_iterator.next();
+            }
+
+        }catch( IOException e ){
+            System.err.println( e );
+        }
+
+        JSONObject champ_wr = new JSONObject( champion_json_string );
+        JSONArray matches = new JSONObject( json_string ).getJSONArray( "matches" );
+
+        for(int i = 0; i < matches.length(); i++) {
+            /* Output parameters */
+            boolean Team1Win = false;
+            double[] champWr = new double[10];
+            double[] playerChampWr = new double[10];
+            double[] playerRecentWr = new double[10];
+            int[] playerChampTimesPlayed = new int[10];
+            int[] playerRanking = new int[10];
+
+            JSONObject match = matches.getJSONObject(i);
+            long match_id = match.getLong( "matchId" );
+            JSONArray teams = match.getJSONArray("teams");
+
+            /* Team1Win */
+
+            if(teams.getJSONObject(0).getBoolean("winner") == true) {
+                Team1Win = true;
+            }
+
+            /* playerChampWr */
+
+            String participantSummonerIds = "";
+
+            JSONArray participants = match.getJSONArray("participants");
+            JSONArray participantsIdentities = match.getJSONArray("participantIdentities");
+            for(int j = 0; j < participantsIdentities.length(); j++) {
+
+                // me gio por el sumoner id y participant id de la identidad
+                long summonerId = participantsIdentities.getJSONObject(j).getJSONObject("player").getLong("summonerId");
+                int participantId = participantsIdentities.getJSONObject(j).getInt("participantId");
+
+                // creo la lista de summoners ids para luego hacer la peticion de ligas
+                if(j == participants.length() - 1)
+                    participantSummonerIds += summonerId;
+                else
+                    participantSummonerIds += summonerId+",";
+
+                // busco el participantid en la lista y saco el champion que juega
+                int participantChampionId = 0;
+                for(int j2 = 0; j2 < participants.length(); j2++) {
+                    if(participants.getJSONObject(j2).getInt("participantId") == participantId) {
+                        participantChampionId = participants.getJSONObject(j2).getInt("championId");
+                        break;
+                    }
+                }
+                // Extraigo de la lista de winrates de cahmpions estatica, el wr del  champion que esta jugando
+                champWr[participantId-1] = champ_wr.getDouble(Integer.toString(participantChampionId));
+
+                // hago la peticion al endpoint de stats para ver el wr y partidas jugadas con ese champ
+                JSONObject participant_stats_by_champ = executeGetRequest(BASE_URL+"v1.3/stats/by-summoner/"+summonerId+"/ranked?season=SEASON2017&api_key="+API_KEY);
+                JSONArray particpant_stats_champ_list = participant_stats_by_champ.getJSONArray("champions");
+
+                // busco el champion que esta jugando en esta partida dentro de la lista de todos lo que ha jugado
+                for(int c = 0; c < particpant_stats_champ_list.length(); c++) {
+
+                    if(particpant_stats_champ_list.getJSONObject(c).getInt("id") == participantChampionId) {
+
+                        JSONObject champ_stats = particpant_stats_champ_list.getJSONObject(c).getJSONObject("stats");
+
+                        playerChampTimesPlayed[participantId-1] = champ_stats.getInt("totalSessionsPlayed");
+                        playerChampWr[participantId-1] = (double)champ_stats.getInt("totalSessionsWon") / (double)champ_stats.getInt("totalSessionsPlayed");
+
+                        break;
+                    }
+                }
+            }
+
+            JSONObject participantsLeagueInfo = executeGetRequest(BASE_URL+"v2.5/league/by-summoner/"+participantSummonerIds+"/entry?api_key="+API_KEY);
+
+            // Vuelvo a recorrer la lista de participantes para encontrar la parte que le corresponde en la respuesta conjunta de las ligas
+            for(int p = 0; p < participantsIdentities.length(); p++) {
+                long summonerId = participantsIdentities.getJSONObject(p).getJSONObject("player").getLong("summonerId");
+                int participantId = participantsIdentities.getJSONObject(p).getInt("participantId");
+                if(participantsLeagueInfo.has(Long.toString(summonerId))) {
+                    // Recorro las diferentes ligas que tiene el jguador
+                    JSONArray leagues = participantsLeagueInfo.getJSONArray(Long.toString(summonerId));
+                    for(int l = 0; l < leagues.length(); l++) {
+                        if(leagues.getJSONObject(l).getString("queue").equals("RANKED_SOLO_5x5")) {
+                            JSONObject pLeagueInfo = leagues.getJSONObject(l);
+                            playerRanking[participantId-1] = getTierMapping(pLeagueInfo.getString("tier")) + getDivisionMapping(pLeagueInfo.getJSONArray("entries").getJSONObject(0).getString("division"));
+                            break;
+                        }
+                    }
+                }
+                else {
+                    System.err.println("League information not found for participant number "+participantId);
+                }
+            }
+
+            System.out.println("Match "+ i +" procesado, guardando...");
+            // Match procesado, procedemos a guardar
+            try {
+                if(Team1Win) {
+                    writer.print("1,");
+                } else {
+                    writer.print("0,");
+                }
+                for(int in = 0; in < champWr.length; in++) {
+                    writer.print(champWr[in]+",");
+                }
+                for(int in = 0; in < playerChampWr.length; in++) {
+                    writer.print(playerChampWr[in]+",");
+                }
+                for(int in = 0; in < playerChampTimesPlayed.length; in++) {
+                    writer.print(playerChampTimesPlayed[in]+",");
+                }
+                for(int in = 0; in < playerRanking.length; in++) {
+                    if(in == 9) {
+                        writer.print(playerRanking[in]+"\n");
+                        break;
+                    }
+                    writer.print(playerRanking[in]+",");
+                }
+            } catch(Exception e) {
+                System.err.println("Error writing match results "+ e.toString());
+            }
+        }
+        System.out.println("Cerrando writer...");
+        writer.close();
+
+    }
+
+    public static int getDivisionMapping( String division ){
+        switch( division ){
+            case "I":
+                return 5;
+            case "II":
+                return 4;
+            case "III":
+                return 3;
+            case "IV":
+                return 2;
+            case "V":
+                return 1;
+            default:
+                return 0;
+        }
+    }
+
+    public static int getTierMapping( String tier ){
+        switch( tier ){
+            case "BRONZE":
+                return 0;
+            case "SILVER":
+                return 6;
+            case "GOLD":
+                return 12;
+            case "PLATINUM":
+                return 18;
+            case "DIAMOND":
+                return 24;
+            case "MASTER":
+                return 30;
+            case "CHALLENGER":
+                return 36;
+        }
+        return 8;
+    }
+
+
+    public static JSONObject executeGetRequest( String url){
+        try{
+            requests += 1;
+            if( requests % 500 == 0 ){
+                TimeUnit.MINUTES.sleep(10);
+            }
+            TimeUnit.SECONDS.sleep(1);
+            URL mh_url = new URL( url );
+            System.out.println( url );
+            HttpURLConnection con = (HttpURLConnection) mh_url.openConnection();
+            int responseCode = con.getResponseCode();
+            if( responseCode != 200 ){
+                /*
+                if(responseCode == 404) {
+                    System.err.println("Response Code " + responseCode + " received.");
+                    return null;
+                }
+                */
+
+                System.err.println( "Response Code " + responseCode + " received. Re issuing request." );
+                TimeUnit.SECONDS.sleep( 20 );
+                return executeGetRequest( url);
+            }
+            BufferedReader in = new BufferedReader( new InputStreamReader( con.getInputStream() ) );
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+            return new JSONObject( response.toString() );
+
+        }catch( Exception e ){
+            System.err.println( e );
+            return null;
+        }
+    }
+
+    public static void usage(){
+        System.err.println( "Usage: JsonFileCleaning <file_name>");
+    }
+
+
+}
---
+++ src/main/java/org/cities/dataextractor/Extractor.java
@@ -0,0 +1,228 @@
+package org.cities.dataextractor;
+
+import org.glassfish.jersey.jackson.JacksonFeature;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.concurrent.TimeUnit;
+
+
+public class Extractor {
+
+    private static final String BASE_URL = "https://euw1.api.riotgames.com/api/lol/euw/";
+    private static final String API_KEY = "6ab6acff-85f6-4580-b127-32067e5667ab";
+    private static int requests = 0;
+
+    public static void main(String[] args) {
+        if(args[0].equals("summoners")) {
+            try {
+                if(args[1] == null || args[2] == null) {
+                    System.out.println("Comando incorrecto: summoners {nº paginas} {out filename}");
+                    return;
+                }
+                extractSummoners(Integer.parseInt(args[1]), args[2]);
+            } catch(Exception e) {
+                System.out.println("Error: "+e.toString());
+            }
+        }
+        else if(args[0].equals("matches")) {
+            try {
+                if(args[1] == null || args[2] == null) {
+                    System.out.println("Comando incorrecto: matches {in filename} {out filename}");
+                    return;
+                }
+                extractMatches(args[1], args[2]);
+            } catch (Exception e) {
+                System.out.println("Error: "+e.toString());
+            }
+
+        }
+        else if(args[0].equals("matches_stats")) {
+            try {
+                if(args[1] == null || args[2] == null) {
+                    System.out.println("Comando incorrecto: matches_stats {in filename} {out filename}");
+                    return;
+                }
+                extractMatchesStats(args[1], args[2]);
+            } catch (Exception e) {
+                System.out.println("Error: "+e.toString());
+            }
+
+        }
+
+    }
+
+    public static void extractSummoners(int num, String outFileName) {
+        System.out.println("Extrayendo lista de summoners:");
+
+        Client client = ClientBuilder.newBuilder().register(JacksonFeature.class).build();
+
+        JSONArray summonersInfo = new JSONArray();
+
+        for(int i = 0; i < num; i++) {
+            JSONObject lolkingresponse = executeGetRequest("http://www.lolking.net/leaderboards/68b9efb3d8cfe899a4e1c17b58e76ec8/euw/"+i+".json");
+            JSONArray summoners = lolkingresponse.getJSONArray("data");
+
+            for(int j = 0; j < summoners.length(); j++) {
+                JSONObject accountInfo  = executeGetRequest("https://euw1.api.riotgames.com/lol/summoner/v3/summoners/"+summoners.getJSONObject(j).getLong("summoner_id")+"?api_key="+API_KEY);
+                summonersInfo.put(accountInfo);
+            }
+        }
+        System.out.println("Guardando json con "+summonersInfo.length()+" summoners");
+        PrintWriter writer = null;
+        try {
+            writer = new PrintWriter(outFileName);
+            writer.print(summonersInfo);
+        } catch(Exception e) {
+            System.out.println(e.toString());
+        } finally {
+            writer.close();
+        }
+    }
+
+    public static void extractMatches(String summonersFileName, String outFileName) {
+
+        JSONArray matches = new JSONArray();
+        String json_string = "";
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(summonersFileName), Charset.forName("ISO-8859-1"));
+            ListIterator<String> lines_iterator = lines.listIterator();
+            while (lines_iterator.hasNext()) {
+                json_string += lines_iterator.next();
+            }
+        } catch (IOException e) {
+            System.out.println("Error: "+e.toString());
+        }
+
+        JSONArray summonersInfo = new JSONArray(json_string);
+
+        for(int i = 0; i < summonersInfo.length(); i++) {
+            long accountId = summonersInfo.getJSONObject(i).getLong("accountId");
+            String accountName = summonersInfo.getJSONObject(i).getString("name");
+            System.out.println("Obteniendo partidas de summoner con accountid: "+ accountId + " - Name: "+accountName);
+            JSONObject matchList = executeGetRequest("https://euw1.api.riotgames.com/lol/match/v3/matchlists/by-account/"+accountId+"/recent?&api_key="+API_KEY);
+            JSONArray summonerMatches = matchList.getJSONArray("matches");
+
+            int summonerAddedMatches = 0;
+            for(int j = 0; j < summonerMatches.length(); j++) {
+                int queue = summonerMatches.getJSONObject(j).getInt("queue");
+                if(queue == 410 || queue == 420 || queue == 440) {
+                    matches.put(summonerMatches.get(j));
+                    summonerAddedMatches++;
+                }
+            }
+            System.out.println("    "+summonerAddedMatches+" partidas extraidas de "+accountName);
+        }
+
+        System.out.println("Guardando json con "+matches.length()+" partidas");
+        PrintWriter writer = null;
+        try {
+            writer = new PrintWriter(outFileName);
+            writer.print(matches);
+        } catch(Exception e) {
+            System.out.println(e.toString());
+        } finally {
+            writer.close();
+        }
+    }
+
+    public static void  extractMatchesStats(String inputFileName, String outFileName) {
+        JSONObject matchesObject = new JSONObject();
+        JSONArray matchesArray = new JSONArray();
+        String json_string = "";
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(inputFileName), Charset.forName("UTF-8"));
+            ListIterator<String> lines_iterator = lines.listIterator();
+            while(lines_iterator.hasNext()) {
+                json_string += lines_iterator.next();
+            }
+        } catch (IOException e) {
+            System.out.println(e.toString());
+            return;
+        }
+        JSONArray jsonArray = new JSONArray(json_string);
+        Client client = ClientBuilder.newBuilder().register(JacksonFeature.class).build();
+
+        for(int i = 0; i<jsonArray.length(); i++) {
+            Object matchId = jsonArray.getJSONObject(i).get("gameId");
+            //System.out.println("https://euw1.api.riotgames.com/lol/euw/v2.2/match/"+matchId.toString()+"?api_key=6ab6acff-85f6-4580-b127-32067e5667ab");
+            Response response = client.target("https://euw1.api.riotgames.com/api/lol/euw/v2.2/match/"+matchId.toString()+"?api_key="+API_KEY).request(MediaType.APPLICATION_JSON).get();
+            if(response.getStatus() != 200) {
+                System.out.println(response.getStatus());
+                continue;
+            }
+            JSONObject matchObject = new JSONObject(response.readEntity(String.class));
+            matchesArray.put(matchObject);
+            if(i%10 == 0 && i != 0) {
+                System.out.println("Tamaño de matches: "+ matchesArray.length());
+            }
+        }
+        matchesObject.put("matches", matchesArray);
+        System.out.println("Guardando json con "+matchesArray.length()+" matches");
+        PrintWriter writer = null;
+        try {
+            writer = new PrintWriter(outFileName);
+            writer.print(matchesObject);
+        } catch(Exception e) {
+            System.out.println(e.toString());
+        } finally {
+            writer.close();
+        }
+
+    }
+
+    public static JSONObject executeGetRequest( String url){
+        try{
+            requests += 1;
+            if( requests % 500 == 0 ){
+                TimeUnit.MINUTES.sleep(10);
+            }
+            TimeUnit.SECONDS.sleep(1);
+            URL mh_url = new URL( url );
+            System.out.println( url );
+            HttpURLConnection con = (HttpURLConnection) mh_url.openConnection();
+            int responseCode = con.getResponseCode();
+            if( responseCode != 200 ){
+                /*
+                if(responseCode == 404) {
+                    System.err.println("Response Code " + responseCode + " received.");
+                    return null;
+                }
+                */
+
+                System.err.println( "Response Code " + responseCode + " received. Re issuing request." );
+                TimeUnit.SECONDS.sleep( 20 );
+                return executeGetRequest( url);
+            }
+            BufferedReader in = new BufferedReader( new InputStreamReader( con.getInputStream() ) );
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+            return new JSONObject( response.toString() );
+
+        }catch( Exception e ){
+            System.err.println( e );
+            return null;
+        }
+    }
+
+}
